1191aa69d449aa6ab6257dbbf707e01a45b51357
thejyotipatel <jyotipatelfyit77100@gmail.com.com>
2022-11-17 20:48:31 +0530

    style templete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { IoIosPeople } from 'react-icons/io'
+// import {
+//   IoMdWallet,
+//   MdLocalOffer,
+//   TbDiscount2,
+//   FaRupeeSign,TfiBook,BiChevronDown,FaUserCircle,BiRupee,BiChevronLeft
+// } from 'react-icons/io'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>Learn React</div>
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = ({ route }) => {
+  return (
+    <footer className=''>
+      <div className='footer-contant'>
+        <h3>Friends Who Enrolled </h3>
+        <h3>Tearms & conditions</h3>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
